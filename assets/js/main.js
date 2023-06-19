d51fe501149941e8768e3f43c50284e78f6a5733
Sachini Vinodya <122256990+sachinivindoya@users.noreply.github.com>
2023-06-19 14:23:06 +0530

    css

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+let typed = new Typed(".typing", {
+    strings: ["", "achini Vinodya"],
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true
+})
+
+// execute after all the content fully loaded
+$(window).on('load', function () {
+    $("#loaderContainer").fadeOut(100);
+    let style = document.createElement("style");
+    style.innerHTML = `body::-webkit-scrollbar {display: block;}`;
+    document.head.appendChild(style);
+});
+
+// run after just creating the dom
+$(document).ready(function () {
+    let style = document.createElement("style");
+    style.innerHTML = `body::-webkit-scrollbar {display: none;}`;
+    document.head.appendChild(style);
+});
